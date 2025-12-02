21cabfad817ef611d3eb374b562e715d72023b20
Nikolay Ivanov <nikolay9619962@mail.ru>
2025-12-02 19:17:10 +0300

    add ex04

---
+++ ex04/ListNode.cs
@@ -0,0 +1,15 @@
+﻿namespace ex04;
+
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+
+    public double val2 { get; }
+
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
---
+++ ex04/Program.cs
@@ -0,0 +1,7 @@
+﻿using ex04;
+
+var l1 = new ListNode(1, null);
+var l2 = new ListNode(2, l1);
+var l3 = new ListNode(3, l2);
+
+                    
