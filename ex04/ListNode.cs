bf672db1805011f64409bab0acd5efb4bcbab408
Nikolay Ivanov <nikolay9619962@mail.ru>
2025-12-09 17:42:43 +0300

    finish

--- ex04/ListNode.cs
+++ ex04/ListNode.cs
@@ -3,14 +3,11 @@
 public class ListNode
 {
     public int val;
-    public ListNode next;
+    public ListNode? next;
 
-    public double val2 { get; }
-
-    public ListNode(int val = 0, ListNode next = null)
+    public ListNode(int val = 0, ListNode? next = null)
     {
         this.val = val;
         this.next = next;
     }
 }
-
